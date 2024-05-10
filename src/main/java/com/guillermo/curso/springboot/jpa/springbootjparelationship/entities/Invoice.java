67dbb62a371ecef728be44cf2ddbb4d4f7a2fde4
Guillermo Fernández <guillefergom@gmail.com>
2024-05-10 12:40:13 +0200

    first commit

---
+++ src/main/java/com/guillermo/curso/springboot/jpa/springbootjparelationship/entities/Invoice.java
@@ -0,0 +1,53 @@
+package com.guillermo.curso.springboot.jpa.springbootjparelationship.entities;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "invoices")
+public class Invoice {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private Long total;
+
+    //El primero de la cardinalidad (Many) se refefrie a la clase (Invoice) muchas facturas -> un cliente
+    @ManyToOne
+    private Client client;
+
+    public Invoice() {
+    }
+
+    public Invoice(String description, Long total) {
+        this.description = description;
+        this.total = total;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getTotal() {
+        return total;
+    }
+
+    public void setTotal(Long total) {
+        this.total = total;
+    }
+}
